048beccacf99612515178e94a7f82a6fb2c0b7e1
Josef Blake <jblake@spotify.com>
2016-07-31 21:25:37 -0400

    use rgb in hash

--- main.js
+++ main.js
@@ -2,51 +2,74 @@
   var window = global;
   var location = window.location;
   var document = global.document;
-  var hasValidHash = false;
+  var preventMouseover = false;
   var isTouchDevice = typeof window.ontouchstart !== 'undefined';
 
-  function updateColor(clientX, clientY) {
+  function calculateRGB(clientX, clientY) {
     var r = Math.min(Math.round(clientX / window.innerWidth * 255), 255);
     var g = Math.min(Math.round(clientY / window.innerHeight * 255), 255);
     var b = Math.round((r + g) / 2);
+    return [r, g, b];
+  }
+
+  function updateColor(rgb) {
+    var r = rgb[0];
+    var g = rgb[1];
+    var b = rgb[2];
     document.body.style.color = 'rgb(' + [r, g, b].join(',') + ')';
     document.body.style.backgroundColor = 'rgb(' + [255 - r, 255 - g, 255 - b].join(',') + ')';
   }
 
   function onClick(e) {
     if (e.target.tagName.toLowerCase() !== 'a') {
-      var clientX = e.clientX;
-      var clientY = e.clientY;
-      location.hash = hasValidHash ? '' : [clientX, clientY].join(',');
-      updateColor(clientX, clientY);
+      location.hash = preventMouseover ? '' : calculateRGB(e.clientX, e.clientY).join(',');
     }
   }
 
   function onHashChange(e) {
     var hash = location.hash.replace(/^#/, '');
 
-    hasValidHash = /[0-9]+,[0-9]/.test(hash);
-
-    if (hasValidHash) {
-      var hashParts = hash.split(',');
-      var clientX = parseInt(hashParts[0], 10);
-      var clientY = parseInt(hashParts[1], 10);
-      updateColor(clientX, clientY);
+    if (!hash) {
+      preventMouseover = false;
+      updateColor([0, 0, 0]);
+      return;
     }
+
+    var hashParts = hash.replace(/^#/, '').split(',');
+
+    if (hashParts.length === 3) {
+      var rgb = hashParts.map(function (str) {
+        return parseInt(str, 10);
+      });
+
+      var isValidRGB = true;
+
+      rgb.forEach(function (val) {
+        if (val < 0 || val > 255) {
+          isValidRGB = false;
+        }
+      });
+
+      if (isValidRGB) {
+        preventMouseover = true;
+        updateColor(rgb);
+        return;
+      }
+    }
+
+    preventMouseover = false;
+    location.hash = '';
   }
 
   function onMouseOver(e) {
-    if (!hasValidHash) {
-      updateColor(e.clientX, e.clientY);
+    if (!preventMouseover) {
+      updateColor(calculateRGB(e.clientX, e.clientY));
     }
   }
 
   function onTouchEnd(e) {
     if (e.target.tagName.toLowerCase() !== 'a') {
-      var clientX = e.changedTouches[0].clientX;
-      var clientY = e.changedTouches[0].clientY;
-      location.hash = [clientX, clientY].join(',');
-      updateColor(clientX, clientY);
+      location.hash = calculateRGB(e.changedTouches[0].clientX, e.changedTouches[0].clientY).join(',');
     }
   }
 
